e66d67148adcedef76eef6576ef5316a81317fea
PPifeas <ppifeas21@gmail.com>
2018-02-18 18:10:11 +0200

    [PP] - Start

---
+++ AirHoop/Assets/Scripts/Monobehavior/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+	private static GameManager instance;
+
+	public static GameManager Instance
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<GameManager>();
+			}
+			return instance;
+		}
+	}
+
+
+	public AircraftScriptable[] aircrafts;
+	public float time;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ AirHoop/Assets/Scripts/ScriptableObjects/AircraftScriptable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AircraftScriptable : ScriptableObject 
+{
+	public string aircraftName;
+	public float aircraftFuel;
+	public float aircraftSpeed;
+	public float aircraftManeuver;
+}
