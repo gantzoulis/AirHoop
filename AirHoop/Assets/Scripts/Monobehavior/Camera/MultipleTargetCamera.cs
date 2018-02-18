783b6679f11c444a3002ad6e76c8597aacb60f61
gantzoulis <gantzoulis@hotmail.com>
2018-02-18 21:56:53 +0200

    [Camera]

---
+++ AirHoop/Assets/Scripts/Monobehavior/Camera/MultipleTargetCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = newPosition;
+    }
+
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
