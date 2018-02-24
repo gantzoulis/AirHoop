d82c44556046d131d6410b0af9eec6fb5a0d3f8b
Anmegil <ppifeas21@gmail.com>
2018-02-25 01:50:03 +0200

    [PP] - EnemySpawnManager initial

--- AirHoop/Assets/Scripts/Monobehavior/Managers/EnemySpawnManager.cs
+++ AirHoop/Assets/Scripts/Monobehavior/Managers/EnemySpawnManager.cs
@@ -5,32 +5,38 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour 
 {
 	public GameObject[] enemy;
+	private int randEnemy;
+	private GameObject spawnEnemy;
 
 	private float randX;
+	private const float distX = 60.0f; 
 	private float randY;
+	private const float distY = 6.0f;
 
 	private Vector3 whereToSpawn;
 
-	public float spawnRate = 2.0f;
+	public float spawnRate = 2.5f;
 	private float nextSpawn = 0.0f;
-
-	void Start()
-	{
-		
-	}
 
 	void Update()
 	{
-		
+		SpawnEngine();
 	}
 
-	private void spawnEngine()
+	private void SpawnEngine()
 	{
 		if(Time.time > nextSpawn)
 		{
-			nextSpawn = Time.time + spawnRate;
-			//randY = Random.Range();
+			randEnemy = Random.Range(0, enemy.Length);
+			spawnEnemy = enemy[randEnemy];
 
+			nextSpawn = Time.time + spawnRate;
+
+			randX = GameManager.Instance.playerObject.transform.position.x + distX;
+			randY = Random.Range(GameManager.Instance.minAirplaneHeight + distY, GameManager.Instance.maxAirplaneHeight);
+			whereToSpawn = new Vector3(randX, randY, 0);
+
+			Instantiate(spawnEnemy, whereToSpawn, Quaternion.identity);
 		}
 	}
 
