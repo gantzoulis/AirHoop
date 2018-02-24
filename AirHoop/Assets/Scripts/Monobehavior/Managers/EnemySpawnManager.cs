8899f26bc8e98e332f3d66e8338f62f924d0a858
Anmegil <ppifeas21@gmail.com>
2018-02-24 23:12:40 +0200

    [P.P] - Waiting for GameManager Update

---
+++ AirHoop/Assets/Scripts/Monobehavior/Managers/EnemySpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour 
+{
+	public GameObject[] enemy;
+
+	private float randX;
+	private float randY;
+
+	private Vector3 whereToSpawn;
+
+	public float spawnRate = 2.0f;
+	private float nextSpawn = 0.0f;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	private void spawnEngine()
+	{
+		if(Time.time > nextSpawn)
+		{
+			nextSpawn = Time.time + spawnRate;
+			randY = Random.Range();
+
+		}
+	}
+
+}
