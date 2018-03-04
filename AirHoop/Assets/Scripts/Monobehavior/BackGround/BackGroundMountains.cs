63f4852424bcc463787f5bb30aa7a84381c9ce70
Anmegil <ppifeas21@gmail.com>
2018-03-04 22:06:40 +0200

    [PP] - Destroy(Deactivate) on mountains/Clouds

---
+++ AirHoop/Assets/Scripts/Monobehavior/BackGround/BackGroundMountains.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMountains : MonoBehaviour 
+{
+	[SerializeField] float destroyTime = 10.0f;
+
+	void Start()
+	{
+		StartCoroutine(DestroyMountain());
+	}
+
+	IEnumerator DestroyMountain()
+	{
+		yield return new WaitForSeconds(destroyTime);
+
+		gameObject.SetActive(false);
+	}
+}
