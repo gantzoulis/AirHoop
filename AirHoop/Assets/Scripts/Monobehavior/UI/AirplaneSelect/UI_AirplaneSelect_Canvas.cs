d396c0bd0981cdff881c8350fb1fc9e68e043f9a
gantzoulis <gantzoulis@hotmail.com>
2018-03-21 00:40:01 +0200

    [GA] Plane Select continue

---
+++ AirHoop/Assets/Scripts/Monobehavior/UI/AirplaneSelect/UI_AirplaneSelect_Canvas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_AirplaneSelect_Canvas : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] airplanePrefabs;
+    private int currentAPselection = 0;
+
+    [SerializeField]
+    private AnimationClip movePlaneLeft;
+    [SerializeField]
+    private AnimationClip movePlaneRight;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ShowNextPlane()
+    {
+        currentAPselection++;
+        airplanePrefabs[currentAPselection - 1].GetComponent<Animator>().SetTrigger("PlayAnimation");
+        airplanePrefabs[currentAPselection].GetComponent<Animator>().SetTrigger("PlayAnimation");
+    }
+
+    public void ShowPrevPlane()
+    {
+        currentAPselection--;
+        airplanePrefabs[currentAPselection + 1].GetComponent<Animator>().SetTrigger("PlayAnimationReverse");
+        airplanePrefabs[currentAPselection].GetComponent<Animator>().SetTrigger("PlayAnimationReverse");
+    }
+}
