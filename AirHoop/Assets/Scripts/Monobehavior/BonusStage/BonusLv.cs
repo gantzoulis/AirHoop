b720c3376eefe4819eaee43420b82ace8b818300
Anmegil <ppifeas21@gmail.com>
2018-03-25 14:12:52 +0300

    [PP] - Bonus Stage Items and Bonus Stage Disable/Points

---
+++ AirHoop/Assets/Scripts/Monobehavior/BonusStage/BonusLv.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusLv : MonoBehaviour 
+{
+	private float counting;
+
+	void Update()
+	{
+		counting = GameObject.Find("SpawnManager").GetComponent<AirSpawnManager>().coundDownSpecialTime; 
+
+		if (counting <= 0)
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
