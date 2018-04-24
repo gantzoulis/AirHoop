3d669e7e6af789a5d91db02e5f52f81b26cb75c9
George <gantzoulis@hotmail.com>
2018-04-24 16:39:21 +0300

    [GA] ServerTalk component

---
+++ AirHoop/Assets/Scripts/Monobehavior/ServerTalk/ServerTalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerTalk : MonoBehaviour
+{
+    [SerializeField]
+    private string serverURL;
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
