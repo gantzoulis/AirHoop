a48f6e96027353ad693197f55821b4f93d2223c0
NewUserName <ppifeas21@gmail.com>
2018-05-13 15:54:16 +0300

    [PP] - SpecialLV Bonus

---
+++ AirHoop/Assets/Scripts/Monobehavior/BonusStage/Hoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop : MonoBehaviour
+{
+    public bool specialHoop = false;
+    public bool passedHoop = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == DataManager.Instance.playerColliderName)
+        {
+            passedHoop = true;
+        }
+    }
+
+    public void ResetHoop()
+    {
+        passedHoop = false;
+    }
+}
---
+++ AirHoop/Assets/Scripts/Monobehavior/BonusStage/SpecialLv.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialLv : MonoBehaviour 
+{
+    private List<GameObject> hoopsList;
+    private bool fullBonusAchieved = false;
+    [SerializeField] private int bonusAwardScore;
+    private bool bonusAwarded = false;
+
+    private float counting;
+
+    void Start()
+    {
+        CreateHoopList();
+    }
+
+    void Update()
+    {
+        CheckIfFullBonusAchieved();
+        AssignBonus();
+    }
+
+    private void DeactivateSpecialLv()
+    {
+        counting = GameObject.Find("SpawnManager").GetComponent<AirSpawnManager>().coundDownSpecialTime;
+
+        if (counting <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void CreateHoopList()
+    {
+        hoopsList = new List<GameObject>();
+
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Hoop")
+            {
+                hoopsList.Add(child.gameObject);
+            }
+        }
+    }
+
+    private void CheckIfFullBonusAchieved()
+    {
+        foreach (GameObject hoop in hoopsList)
+        {
+            if (hoop.GetComponentInChildren<Hoop>().passedHoop == false)
+            {
+                fullBonusAchieved = false;
+                return;
+            }
+            else
+            {
+                fullBonusAchieved = true;
+            }
+        }
+    }
+
+    private void AssignBonus()
+    {
+        if (fullBonusAchieved && !bonusAwarded)
+        {
+            DataManager.Instance.playerScore += bonusAwardScore;
+            Debug.Log("Full Bonus Achieved");
+            bonusAwarded = true;
+        }
+    }
+
+}
