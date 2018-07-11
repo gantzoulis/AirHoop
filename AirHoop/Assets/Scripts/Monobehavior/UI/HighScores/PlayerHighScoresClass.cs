08ece978f5d74e146f399efd9e264368cde5ca5a
George <gantzoulis@hotmail.com>
2018-07-11 17:24:36 +0300

    [GA] Updated the Playground for some cool stuff. + Highscores UI

---
+++ AirHoop/Assets/Scripts/Monobehavior/UI/HighScores/PlayerHighScoresClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerHighScoresClass
+{
+    public string playerName;
+    public int playerMaxDistance;
+    public int playerTotalFlightTime;
+}
+
