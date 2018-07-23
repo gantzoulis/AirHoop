d7f26853019500d843381a6a61bf89e3988758bb
George <gantzoulis@hotmail.com>
2018-07-23 12:47:07 +0300

    [GA] Testing new Unity 2018

---
+++ AirHoop/Assets/PLAYGROUND/HighScoresUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoresUI : MonoBehaviour
+{
+
+	void OnEnable()
+    {
+        debug.log("UI Enabled");
+    }
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+     
+}
