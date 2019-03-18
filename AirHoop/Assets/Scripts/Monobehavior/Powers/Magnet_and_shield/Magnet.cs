f22002a9bf80d3035532c0bd8c65a9f0119e173f
gantzoulis <gantzoulis@hotmail.com>
2019-03-18 17:15:21 +0200

    [GA] Forcefield & Lightweight Render Pipeline upgrade - Please Check compatibility.

---
+++ AirHoop/Assets/Scripts/Monobehavior/Powers/Magnet_and_shield/Magnet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+
+    public Transform playerObject;
+    public float magnetSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = Vector3.MoveTowards(transform.position, playerObject.position, magnetSpeed * Time.deltaTime);
+	}
+}
---
+++ AirHoop/Assets/Scripts/Monobehavior/Powers/Magnet_and_shield/MagnetTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetTrigger : MonoBehaviour
+{
+
+    public float magnetSetSpeed;
+    public GameObject currentTarget;
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        other.gameObject.GetComponent<Magnet>().magnetSpeed = magnetSetSpeed;
+    }
+}
